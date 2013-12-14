63bcba11555da9643ed9db44a31ddcdd75701fe3
Trevor Whitney <trevorjwhitney@gmail.com>
2013-12-14 12:58:42 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyVime::Application.config.secret_key_base = 'd901ddc30c308eafbf9446d36f7887ee53c10072c3dd218c9ece9b50ba3196b6348eef329ad29f7e2025b95db12158d711aa4565acbb75de93d0ad75f28641e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyVime::Application.config.session_store :cookie_store, key: '_RubyVime_session'
